1a5a50969d7269f67abe0d5a7e8ba56a9c5eb36f
I am Tom <120075730+iamtomnotjerry@users.noreply.github.com>
2023-12-14 14:03:38 +0700

    a

--- src/app/ui/books/AddtoCart.tsx
+++ src/app/ui/books/AddtoCart.tsx
@@ -1,34 +1,41 @@
-'use client'
+// 'use client'
 
-import { useContext } from "react";
+// import { useContext } from "react";
+// // import { StoreContext } from "@/app/context";
+// import { ShoppingCartIcon } from "@heroicons/react/24/solid";
+// import { toast } from 'react-toastify';
 // import { StoreContext } from "@/app/context";
-import { ShoppingCartIcon } from "@heroicons/react/24/solid";
-import { toast } from 'react-toastify';
-import { StoreContext } from "@/app/context";
-const AddToCart = ({book}) => {
-  const {cartData, setCartData} = useContext(StoreContext);
+// const AddToCart = ({book}) => {
+//   const {cartData, setCartData} = useContext(StoreContext);
 
-  const handleCart = (e, reason) => {
-    e.preventDefault();
-    const newData = {...book, type: reason};
-    setCartData([...cartData,newData]);
-    toast.success(`Added ${newData.title} to the cart`,{
-        autoClose: 1000,
-        position: toast.POSITION.TOP_RIGHT
-    })
-}
+//   const handleCart = (e, reason) => {
+//     e.preventDefault();
+//     const newData = {...book, type: reason};
+//     setCartData([...cartData,newData]);
+//     toast.success(`Added ${newData.title} to the cart`,{
+//         autoClose: 1000,
+//         position: toast.POSITION.TOP_RIGHT
+//     })
+// }
 
 
   
-  return (
-    <div className="flex justify-around">
-      <button 
-        className="bg-purple-500  hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
-        onClick = {(event) => handleCart(event, 'Buy')}>
-        <ShoppingCartIcon className="w-5 mr-0.5 inline" /> Add To Cart
-      </button>
-    </div>
-  );
-};
+//   return (
+//     <div className="flex justify-around">
+//       <button 
+//         className="bg-purple-500  hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+//         onClick = {(event) => handleCart(event, 'Buy')}>
+//         <ShoppingCartIcon className="w-5 mr-0.5 inline" /> Add To Cart
+//       </button>
+//     </div>
+//   );
+// };
 
-export default AddToCart;
+// export default AddToCart;
+import React from 'react'
+
+export default function AddtoCart() {
+  return (
+    <div>AddtoCart</div>
+  )
+}
