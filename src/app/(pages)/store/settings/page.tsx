3c67283fda71e81606f6e18c629952d436d7df08
I am Tom <120075730+iamtomnotjerry@users.noreply.github.com>
2023-12-14 13:52:59 +0700

    a

--- src/app/(pages)/store/settings/page.tsx
+++ src/app/(pages)/store/settings/page.tsx
@@ -9,9 +9,9 @@ export default function Settings() {
     <div className="p-4 max-w-md mx-auto">
       <h2 className="text-3xl font-semibold mb-4">Account Settings</h2>
 
-      <SettingsOption title="Change Password" description="Update your account password" />
+      <SettingsOption title="Change Password"  />
 
-      <SettingsOption title="Delete Account" description="Permanently delete your account" isDestructive />
+      <SettingsOption title="Delete Account" isDestructive />
     </div>
   );
 }
