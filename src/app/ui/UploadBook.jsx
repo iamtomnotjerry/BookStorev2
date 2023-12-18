5e84fdb3721603f873ab163612bba89698c8fbee
I am Tom <120075730+iamtomnotjerry@users.noreply.github.com>
2023-12-18 09:58:37 +0700

    quick fix

--- src/app/ui/UploadBook.jsx
+++ src/app/ui/UploadBook.jsx
@@ -19,7 +19,7 @@ export default function SellTable() {
   const { data: session } = useSession();
 
   // Handle form input changes
-  const handleChange = (e:any) => {
+  const handleChange = (e) => {
     const { name, value, type } = e.target;
 
     // Check if the input is a file input
@@ -33,7 +33,7 @@ export default function SellTable() {
   };
 
   // Handle form submission
-  const handleSubmit = async (e:any) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
