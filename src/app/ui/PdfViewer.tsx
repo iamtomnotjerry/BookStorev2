bee30dd148fa3be0e655b929e6f04932d13cc0f1
I am Tom <120075730+iamtomnotjerry@users.noreply.github.com>
2023-12-20 16:11:19 +0700

    a

--- src/app/ui/PdfViewer.tsx
+++ src/app/ui/PdfViewer.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
+
+// Set workerSrc once in your application
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 interface PdfViewerProps {
@@ -7,18 +9,45 @@ interface PdfViewerProps {
 }
 
 const PdfViewer: React.FC<PdfViewerProps> = ({ pdfUrl }) => {
-  const [numPages, setNumPages] = React.useState<number | null>(null);
+  const [numPages, setNumPages] = useState<number | null>(null);
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
 
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
+  const updateWindowWidth = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowWidth);
+    return () => {
+      window.removeEventListener('resize', updateWindowWidth);
+    };
+  }, []);
+
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages);
-  }
+  };
 
   return (
     <div style={{ width: '100%', height: '100vh', overflowX: 'hidden' }}>
       <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
         {Array.from(new Array(numPages || 0), (el, index) => (
-          <div key={`page_${index + 1}`} style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <Page pageNumber={index + 1} renderMode="canvas" renderTextLayer={false} renderAnnotationLayer={false} width={window.innerWidth * 0.8} />
+          <div
+            key={`page_${index + 1}`}
+            style={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Page
+              pageNumber={index + 1}
+              renderMode="canvas"
+              renderTextLayer={false}
+              renderAnnotationLayer={false}
+              width={windowWidth * 0.8}
+            />
           </div>
         ))}
       </Document>
