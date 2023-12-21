45c9fb4b30cf79f6be240fb373f0edeef2c7d0d3
I am Tom <120075730+iamtomnotjerry@users.noreply.github.com>
2023-12-21 10:17:42 +0700

    a

--- src/app/ui/PdfViewer.tsx
+++ src/app/ui/PdfViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css'; // Import the annotation layer styles
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -9,6 +9,28 @@ interface PdfViewerProps {
 
 const PdfViewer: React.FC<PdfViewerProps> = ({ pdfUrl }) => {
   const [numPages, setNumPages] = useState<number | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        if (entry.contentRect.width !== containerWidth) {
+          setContainerWidth(entry.contentRect.width);
+        }
+      }
+    });
+
+    if (containerRef.current) {
+      resizeObserver.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        resizeObserver.unobserve(containerRef.current);
+      }
+    };
+  }, [containerWidth]);
 
   // Function to handle document load success
   const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
@@ -16,10 +38,30 @@ const PdfViewer: React.FC<PdfViewerProps> = ({ pdfUrl }) => {
   };
 
   return (
-    <div>
+    <div
+      ref={containerRef}
+      style={{ width: '100%', height: '100vh', overflowX: 'hidden' }}
+    >
       <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
-        {Array.from(new Array(numPages), (el, index) => (
-          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        {Array.from(new Array(numPages || 0), (el, index) => (
+          <div
+            key={`page_${index + 1}`}
+            style={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Page
+              key={`page_${index + 1}`}
+              pageNumber={index + 1}
+              renderMode="canvas"
+              renderTextLayer={false}
+              width={containerWidth * 0.8} // Adjust the width based on the container width
+            />
+          </div>
         ))}
       </Document>
     </div>
