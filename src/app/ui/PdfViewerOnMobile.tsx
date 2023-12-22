2affb3200c1d800d5e9e0f392a3f3bccf4d217d6
I am Tom <120075730+iamtomnotjerry@users.noreply.github.com>
2023-12-22 11:16:46 +0700

    a

--- src/app/ui/PdfViewerOnMobile.tsx
+++ src/app/ui/PdfViewerOnMobile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css'; // Import the annotation layer styles
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -16,26 +16,21 @@ const PdfViewerOnMobile: React.FC<PdfViewerProps> = ({ pdfUrl }) => {
   };
 
   return (
-    <div
-      style={{ width: '100%', height: '100vh', overflowX: 'hidden' }}
-    >
+    <div style={{ width: '100%', height: '100vh', overflow: 'auto' }}>
       <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
         {Array.from(new Array(numPages || 0), (el, index) => (
           <div
             key={`page_${index + 1}`}
             style={{
               width: '100%',
-              height: '100%',
               display: 'flex',
               justifyContent: 'center',
-              alignItems: 'center',
             }}
           >
             <Page
               key={`page_${index + 1}`}
               pageNumber={index + 1}
-              renderMode="canvas"
-              renderTextLayer={false}
+              width={300} // Adjust the width of the page as needed
             />
           </div>
         ))}
@@ -45,5 +40,3 @@ const PdfViewerOnMobile: React.FC<PdfViewerProps> = ({ pdfUrl }) => {
 };
 
 export default PdfViewerOnMobile;
-
-
